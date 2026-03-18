fefd3fd6eb9b139fd0553d2cd750f0c3a095c249
henryagunanne <henryagunanne@gmail.com>
2026-03-18 19:54:26 +0800

    //

---
+++ includes/initial.php
@@ -0,0 +1,26 @@
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Management</title>
+        <link rel="stylesheet" href="style.css">
+
+        <!-- fonts -->
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&family=Pixelify+Sans:wght@400..700&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&family=Notable&family=Pixelify+Sans:wght@400..700&display=swap" rel="stylesheet">
+    </head>
+
+    <body>
+        <!-- NAVIGATION -->
+        <header>
+            <nav>
+                <select class>
+                    <option>Manila Branch</option>
+                    <option>Laguna Branch</option>
+                </select>
+                <img src="images/logo.png" class="logo" alt="Cool Beans Logo" />
+                <a href=""><img src="images/user.png" class="icons" /></a>
+            </nav>
+        </header>
