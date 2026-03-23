212d5c57c4ef97409426948986af5b23aced7e00
Martina Mari Andaya <martina.mari.andaya@gmail.com>
2026-03-23 23:44:40 +0800

    Update db-connect.php

--- coffee-backend/config/db-connect.php
+++ coffee-backend/config/db-connect.php
@@ -1,16 +1,13 @@
 <?php
-    // Database credentials
-    $host = "localhost"; // VM IP
-    $port = 3306;
-    $database = "cool_beans";
-    $username = "student1";
-    $password = "Dlsu1234!";
+$host = "localhost";  
+$port = 3307;   
+$database = "cool_beans";
+$username = "student1";
+$password = "Dlsu1234!";
 
-    // Create connection
-    $conn = new mysqli($host, $username, $password, $database, $port);
+$conn = new mysqli($host, $username, $password, $database, $port);
 
-    // Check connection
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-    }
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
 ?>
