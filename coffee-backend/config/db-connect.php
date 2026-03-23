2110eb8994df2d7d717411487acb2f39b2f29445
henryagunanne <henryagunanne@gmail.com>
2026-03-24 00:51:16 +0800

    VM Details on db connect

--- coffee-backend/config/db-connect.php
+++ coffee-backend/config/db-connect.php
@@ -1,14 +1,14 @@
 <?php
     // Database credentials
-    $host = "localhost"; // VM IP
-    $port = 3307;
+    $host = "10.2.0.17"; // VM IP
+    $port = 22017;
     $database = "cool_beans";
-    $username = "student1";
-    $password = "Dlsu1234!";
+    $username = "root";
+    $password = "RqxWHOg1ZJfY";
 
-$conn = new mysqli($host, $username, $password, $database, $port);
+    $conn = new mysqli($host, $username, $password, $database, $port);
 
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
 ?>
